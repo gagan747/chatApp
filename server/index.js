cb70b98571a34f00e9433ebb9355233d8f6ae0f7
gurjot74 <gagan.singh@tothenew.com>
2023-01-20 14:40:52 +0530

    iitial commit

--- server/index.js
+++ server/index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 const { Server } = require('socket.io');
 const { createServer } = require('http');
 const httpServer = createServer(app);
+const root = path.join(__dirname, '../', 'client', 'build', 'index.html')
 const io = new Server(httpServer, {
 	cors: {
 		origin: '*',
 		credentials: true,
 	},
+});
+console.log(__dirname)
+app.use(express.static('../client/build'));
+app.get('*', (req, res) => {
+	res.sendFile( root );
 });
 io.on('connection', (socket) => {
 	console.log(socket.id);
